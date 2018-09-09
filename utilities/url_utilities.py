9e058d196b372e93ad49ff55608a897f36664bc1
Eduardo <eduardos@spotify.com>
2018-09-10 00:42:31 +0200

    initial commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def load_urls_from_files(file_path: str):
+    #TODO: add the code needed to read the text file with the urls in it
+    pass
+
+def load_page(url: str):
+    #TODO: add the code that reads th url
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: analyze the text
+    pass
